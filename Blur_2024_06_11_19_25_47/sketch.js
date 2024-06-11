079fd63e4f16e877b482931abd3ce012c7d5e12a
fnpjups <144931469+fnpjups@users.noreply.github.com>
2024-06-11 23:45:49 +0400

    Add files via upload

---
+++ Blur_2024_06_11_19_25_47/sketch.js
@@ -0,0 +1,14 @@
+var img;
+function preload(){
+  img = loadImage('20230706_190401.jpg');
+}
+
+function setup() {
+  createCanvas(500, 430);
+  background(0);
+}
+
+function draw() {
+  image(img, 125, 0, 250, 430);
+  filter(BLUR);
+}
